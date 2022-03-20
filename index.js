8b227edbfa26c172e6647d0fd4ee20433f49ccb5
Ahsan Irfan <ahsan.irfan@gmail.com>
2022-03-20 14:55:16 -0400

    Put up an ejs template for informing the user that they are not logged in and that they should login or register.

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-// Requiring Dot ENV to load environment variables into process.env
-require('dotenv').config()
+// Web Serve Configuration
 
-// Setting PORT
-const PORT = process.env.PORT;
+  // (1) Requiring Dot ENV to load environment variables into process.env
+  require('dotenv').config()
 
-// Requiring & Mounting Express - Required to run Node Apps.
-const express = require("express");
-const app = express();
+  // (2) Setting PORT
+  const PORT = process.env.PORT;
 
-// Requiring Body Parser - Required to parse req.body, which is how the body of the HTTP request can be viewed.
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+  // (3) Requiring & Mounting Express - Required to run Node Apps.
+  const express = require("express");
+  const app = express();
 
-// Requiring BCrypt - Required for password security.
-const bcrypt = require("bcryptjs");
+  // (4) Requiring Body Parser - Required to parse req.body, which is how the body of the HTTP request can be viewed.
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({extended: true}));
 
-// Requiring Cookie Session - Required for encrypting cookies.
-const cookieSession = require("cookie-session");
-app.use(cookieSession({
-  keys: ["mynameisahsan"] // Key required to ecrypt and decrypt the cookies. Does this need to be in a .env file?
-}));
+  // (5) Requiring BCrypt - Required for password security.
+  const bcrypt = require("bcryptjs");
 
-// Mounting the EJS Template
-app.set("view engine", "ejs");
+  // (6) Requiring Cookie Session - Required for encrypting cookies.
+  const cookieSession = require("cookie-session");
+  app.use(cookieSession({keys: ["mynameisahsan"]})); // Key required to ecrypt and decrypt the cookies. Does this need to be in a .env file?
+
+  // (7) Mounting the EJS Template
+  app.set("view engine", "ejs");
+
+// HTTP Routes
+
+  // (1) GET Requests
+
+    // (A) When you first access the website
+    app.get("/", (req, res) => {
+    const templateVars = {
+
+    }
+    res.render("unlogged", templateVars);
+    });
 
 // Listen Function
 app.listen(PORT, ()=> {
